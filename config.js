a14a9a98ce27f48dcc623fb4e9f03fd786c38589
Benjamin Lupton <b@lupton.cc>
2016-10-29 12:53:40 +0800

    fix syntax

--- config.js
+++ config.js
@@ -1,31 +1,29 @@
-module.exports = [
-	{
-		nickname: process.env.SLACKIRC_NICKNAME || 'slackirc',
-		server: process.env.SLACKIRC_SERVER || 'chat.freenode.net',
-		token: process.env.SLACKIRC_TOKEN,
-		channelMapping: (function () {
-			var channels = {}
-			process.env.SLACKIRC_CHANNELS.split(',').forEach(function (value) {
-				var parts = value.split(':')
-				if ( parts.length === 1 ) {
-					var part = parts[0].trim()
-					channels[part] = part
-				}
-				else if ( parts.length === 2 ) {
-					var slack = parts[0].trim()
-					var irc = parts[1].trim()
-					channels[slack] = irc
-				}
-			})
-			return channels
-		})()
-	},
+module.exports = [{
+	nickname: process.env.SLACKIRC_NICKNAME || 'slackirc',
+	server: process.env.SLACKIRC_SERVER || 'chat.freenode.net',
+	token: process.env.SLACKIRC_TOKEN,
+	channelMapping: (function () {
+		var channels = {}
+		process.env.SLACKIRC_CHANNELS.split(',').forEach(function (value) {
+			var parts = value.split(':')
+			if ( parts.length === 1 ) {
+				var part = parts[0].trim()
+				channels[part] = part
+			}
+			else if ( parts.length === 2 ) {
+				var slack = parts[0].trim()
+				var irc = parts[1].trim()
+				channels[slack] = irc
+			}
+		})
+		return channels
+	})(),
 	autoSendCommands: process.env.SLACKIRC_PASSWORD && [
-      ['PRIVMSG', 'NickServ', 'IDENTIFY ' + process.env.SLACKIRC_PASSWORD],
-      ['MODE', 'test', '+x'],
-      ['AUTH', 'test', 'password']
-  	] || null,
-]
+		['PRIVMSG', 'NickServ', 'IDENTIFY ' + process.env.SLACKIRC_PASSWORD],
+		['MODE', 'test', '+x'],
+		['AUTH', 'test', 'password']
+	] || null
+}]
 
 // Debug
 console.log('Starting with configuration:', module.exports)
