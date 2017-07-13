af31f3963501fb6baafe6934612b7c8ea114e663
Benjamin Lupton <b@lupton.cc>
2017-07-13 14:25:25 +0800

    add more log details

--- config.js
+++ config.js
@@ -35,4 +35,6 @@ module.exports = [{
 }]
 
 // Debug
+console.log('Starting with versions:', JSON.stringify(process.versions, null, '  '))
+console.log('Starting with environment configuration:', JSON.stringify(process.env, null, '  '))
 console.log('Starting with configuration:', JSON.stringify(module.exports, null, '  '))
